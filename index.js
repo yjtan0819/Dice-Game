006b02e333ac81c7913d2747adce1d45872bdb96
yjtan0819 <tan.yijia0819@gmail.com>
2023-01-10 21:50:30 -0500

    Update files

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ image1.setAttribute("src", randomImageSource);
 image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
-    } 
-else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
-    }
-else {
-    document.querySelector("h1").innerHTML = "Draw!";
-    }
-
-
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
